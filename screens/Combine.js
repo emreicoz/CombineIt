ae2251d586343944cc21b092c114cfe9a887632c
emreicoz <emreicoz1905@gmail.com>
2021-04-28 16:42:42 +0300

    Custom color picker added

--- screens/Combine.js
+++ screens/Combine.js
@@ -1,16 +1,15 @@
-import React, { useContext } from 'react';
-import {View,Image} from 'react-native';
+import React, {useContext, useState} from 'react';
+import {View, Image} from 'react-native';
 import {Text} from 'native-base';
-import { UserContext } from '../elements/UserContext';
+import {UserContext} from '../elements/UserContext';
 
-export default function Combine () {
-    const {user} = useContext(UserContext);
-    console.log("Kombin tarafı: " +user);
-    return(
-        <View>
-            <Text>
-                Kombin sayfası. 
-            </Text>
-        </View>
-    )
+export default function Combine() {
+  const {user} = useContext(UserContext);
+  console.log('Kombin tarafı: ' + user);
+
+  return (
+    <View>
+      <Text>Kombin sayfası.</Text>
+    </View>
+  );
 }
--- screens/NewClothe.js
+++ screens/NewClothe.js
@@ -1,5 +1,14 @@
 import React, {useState} from 'react';
-import {Text, View, TouchableOpacity, StyleSheet, Image} from 'react-native';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  StyleSheet,
+  Image,
+  FlatList,
+  Modal,
+} from 'react-native';
 import ImagePickerModal from '../elements/ImagePickerModal';
 import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
@@ -51,6 +60,47 @@ export default function NewClothe() {
       }
     });
   };
+  const colors = [
+    {
+      color: '#154',
+    },
+    {
+      color: '#547',
+    },
+    {
+      color: '#215',
+    },
+    {
+      color: '#985',
+    },
+    {
+      color: '#abf',
+    },
+    {
+      color: '#acd',
+    },
+    {
+      color: '#154',
+    },
+    {
+      color: '#547',
+    },
+    {
+      color: '#215',
+    },
+    {
+      color: '#985',
+    },
+    {
+      color: '#abf',
+    },
+    {
+      color: '#acd',
+    },
+  ];
+  const [selectedColor, setSelectedColor] = useState('#fff');
+  const [colorModalVisible, setColorModalVisible] = useState(false);
+
   return (
     <View style={styles.container}>
       <View style={styles.profileImageContainer}>
@@ -78,6 +128,56 @@ export default function NewClothe() {
       <View style={styles.attributes}>
         <View style={styles.attribute}>
           <Text>Renk: </Text>
+          <Modal
+            animationType="fade"
+            transparent={true}
+            visible={colorModalVisible}
+            onRequestClose={() => {
+              setColorModalVisible(false);
+            }}>
+            <View style={{backgroundColor: '#000000aa', flex: 1}}>
+              <TouchableOpacity
+                style={{flex: 1}}
+                onPress={() => setColorModalVisible(false)}>
+                <TouchableWithoutFeedback>
+                  <View style={styles.modal}>
+                    <FlatList
+                      key="#"
+                      data={colors}
+                      numColumns={5}
+                      keyExtractor={(item, index) => item + index}
+                      renderItem={({item}) => (
+                        <View style={{padding: 2}}>
+                          <TouchableOpacity
+                            onPress={() => {
+                              setSelectedColor(item.color);
+                              setColorModalVisible(false);
+                            }}>
+                            <View
+                              style={{
+                                width: 30,
+                                height: 30,
+                                backgroundColor: item.color,
+                                borderRadius: 50,
+                              }}></View>
+                          </TouchableOpacity>
+                        </View>
+                      )}
+                    />
+                  </View>
+                </TouchableWithoutFeedback>
+              </TouchableOpacity>
+            </View>
+          </Modal>
+          <TouchableOpacity onPress={() => setColorModalVisible(true)}>
+            <View
+              style={{
+                width: 30,
+                height: 30,
+                backgroundColor: selectedColor,
+                borderRadius: 50,
+              }}></View>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
@@ -99,5 +199,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#95a5a6',
     borderRadius: 20,
     borderColor: 'gray',
+  },
+  attribute: {
+    flexDirection: 'row',
+  },
+  modal: {
+    marginVertical: '50%',
+    marginHorizontal: '24%',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 15,
+    alignItems: 'center',
+  },
+  colorView: {
+    width: 30,
+    height: 30,
+    backgroundColor: 'white',
+    borderRadius: 50,
   },
 });
