c5b7b768f5231ab346762862a90b229aa8475f34
emreicoz <emreicoz1905@gmail.com>
2021-06-02 15:08:38 +0300

    Combine Screen

--- screens/Combine.js
+++ screens/Combine.js
@@ -8,21 +8,6 @@ export default function Combine({navigation}) {
   console.log('Kombin tarafı: ' + user);
   var deneme = "";
 
-  /*const {spawn} = require('child_process');
-
-  const childPython = spawn('python',['--version']);
-
-  childPython.stdout.on('data', (data) => {
-    deneme = data;
-  })
-
-  childPython.stderr.on('data', (data)=> {
-    console.log('stderr: ' ,data);
-  })
-
-  childPython.on('close', (code)=>{
-    console.log('child process executed with code: ',code);
-  })*/
 
   return (
     <View>
