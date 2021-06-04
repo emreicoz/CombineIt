40e2a3c2157e3ddd1e2cec6abe2e96a69bfdf8d9
emreicoz <emreicoz1905@gmail.com>
2021-06-04 16:57:36 +0300

    App and Api integration completed.

--- screens/Combine.js
+++ screens/Combine.js
@@ -6,13 +6,45 @@ import {UserContext} from '../elements/UserContext';
 export default function Combine({navigation}) {
   const {user} = useContext(UserContext);
   console.log('Kombin tarafı: ' + user);
-  var deneme = "";
+  const [apiData, setApiData] = useState();
 
+  const getApi = async () => {
+    try {
+      await fetch('https://combineit.pythonanywhere.com/post', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          firstParam: 'yourValue',
+          secondParam: {
+            lol: 'loll',
+            hoh: 'hohh'
+          } 
+        }),
+      })
+        .then(response => response.json())
+        .then(json => {
+          setApiData(json);
+          console.log(json);
+          return json.movies;
+          
+        });
+    } catch (error) {
+      console.error('ERror : ', error);
+    }
+  };
+
+  useEffect(() => {
+    console.log('Api a ulaşıldı');
+    getApi();
+  }, []);
 
   return (
     <View>
       <Text>Kombin sayfası. </Text>
-      <Text>Deneme: {deneme} </Text>
+      <Text>Deneme: {apiData ? apiData.data.firstParam : 'Yok'} </Text>
       <TextInput placeholder="deneme" />
     </View>
   );
